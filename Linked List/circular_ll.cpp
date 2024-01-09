4cfdc09e19e758b1157b18516ec357d456b74cef
Umair Inayat <68996836+umairinayat@users.noreply.github.com>
2024-01-10 02:28:11 +0500

    Update circular_ll.cpp

--- Linked List/circular_ll.cpp
+++ Linked List/circular_ll.cpp
@@ -1,32 +1,3 @@
-// #include<iostream>
-
-// using namespace std;
-// class Node
-// {
-
-// public:
-// int data;
-// Node *next;
-//     Node(int data){
-//         this->data=data;
-//         next=nullptr;
-//     }
-//     Node(){
-//         next=nullptr;
-//     }
-//     ~Node(){
-//         delete next;
-//     }
-// };
-
-
-
-// int main() {
-    
-
-//     cout << endl ;
-//     return 0;
-// }
 
 #include <iostream>
 
