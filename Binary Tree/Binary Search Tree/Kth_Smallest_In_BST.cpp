9bf141032f98fe51ec8eed1e2ca57489c844fbc4
Umair Inayat <68996836+umairinayat@users.noreply.github.com>
2024-01-10 23:45:55 +0500

    Create Kth_Smallest_In_BST.cpp

---
+++ Binary Tree/Binary Search Tree/Kth_Smallest_In_BST.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+#include "BST.h" //include BST functions
+using namespace std;
+
+int kthSmallest(Node* root, int &index, int K) {
+    if(root == NULL) {
+        return -1;
+    }
+
+    int left = kthSmallest(root->left, index, K);
+
+    if(left != -1) {
+        return left;
+    }
+
+    index++;
+    if(index == K) {
+        return root->data;
+    }
+
+    return kthSmallest(root->right, index, K);
+}
+
+int main() {
+    Node* root = NULL;
+    int K, ans;
+
+    cout << "Enter data to create BST : ";
+    takeinput(root);
+
+    cout << "Enter the value of K : ";
+    cin >> K;
+
+    cout << endl << "Level Order Traversal : " << endl;
+    levelOrderTraversal(root);
+
+    int index = 0;
+    ans = kthSmallest(root, index, K);
+
+    cout << "Required answer = " << ans << endl;
+    return 0;
+}
